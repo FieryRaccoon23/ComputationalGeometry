161381f84e16b491bbd4535dac23033843516743
Araib <araib.saleem@gmail.com>
2025-06-21 11:58:53 -0400

    Moved all directories setting to CMakeList.txt to easily manage

--- run_cmake.sh
+++ run_cmake.sh
@@ -4,7 +4,7 @@ SOURCE_FOLDER=${1:-src}  # default to 'src' if not passed
 OUTPUT_FOLDER="build"
 
 echo "🔧 Running CMake generation..."
-cmake -DCGAL_DIR=$PWD/libs/CGAL-6.0.1 -DCMAKE_BUILD_TYPE=Release -DBOOST_ROOT=$PWD/libs/boost_1_88_0 -DSOURCE_FOLDER="$SOURCE_FOLDER" -DOUTPUT_FOLDER="$OUTPUT_FOLDER" -B "$OUTPUT_FOLDER"
+cmake -DCMAKE_BUILD_TYPE=Release -DSOURCE_FOLDER="$SOURCE_FOLDER" -DOUTPUT_FOLDER="$OUTPUT_FOLDER" -B "$OUTPUT_FOLDER"
 
 LINK_FILE="$OUTPUT_FOLDER/CMakeFiles/app.dir/link.txt"
 
@@ -15,4 +15,4 @@ else
   echo "⚠️ $LINK_FILE not found — skipping -lFALSE cleanup"
 fi
 
-echo "✅ CMake setup complete. You can now run: make -C $OUTPUT_FOLDER"
+echo "✅ CMake setup complete. You can now run: ./make.sh"
--- run_cmake_debug.sh
+++ run_cmake_debug.sh
@@ -4,7 +4,7 @@ SOURCE_FOLDER=${1:-src}  # default to 'src' if not passed
 OUTPUT_FOLDER="buildDebug"
 
 echo "🔧 Running CMake generation for Debug..."
-cmake -DCGAL_DIR=$PWD/libs/CGAL-6.0.1 -DCMAKE_BUILD_TYPE=Debug -DBOOST_ROOT=$PWD/libs/boost_1_88_0 -DSOURCE_FOLDER="$SOURCE_FOLDER" -DOUTPUT_FOLDER="$OUTPUT_FOLDER" -B "$OUTPUT_FOLDER"
+cmake -DCMAKE_BUILD_TYPE=Debug -DSOURCE_FOLDER="$SOURCE_FOLDER" -DOUTPUT_FOLDER="$OUTPUT_FOLDER" -B "$OUTPUT_FOLDER"
 
 LINK_FILE="$OUTPUT_FOLDER/CMakeFiles/app.dir/link.txt"
 
